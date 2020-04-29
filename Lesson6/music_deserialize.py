130b5eb6106d34eac8c555ed6bab265a71d388d5
alex13th <alex13th76@gmail.com>
2020-04-29 10:40:51 +0800

    Lesson 6 is done

---
+++ Lesson6/music_deserialize.py
@@ -0,0 +1,16 @@
+"""
+Создать модуль music_deserialize.py.
+    * В этом модуле открыть файлы group.json и group.pickle, прочитать из них информацию. 
+    * И получить объект: словарь из предыдущего задания.
+"""
+
+import json
+import pickle
+
+with open("group.json", "r", encoding="utf-8") as f_json:
+    group_json = json.load(f_json)
+print("JSON:", group_json)
+    
+with open("group.pickle", "rb") as f_pickle:
+    group_pickle = pickle.load(f_pickle)
+print("PICLE:", group_pickle)
---
+++ Lesson6/music_serialize.py
@@ -0,0 +1,35 @@
+"""
+Создать модуль *music_serialize.py*. 
+    В этом модуле определить словарь для вашей любимой музыкальной группы, например:
+    my_favourite_group = {
+    ‘name’: ‘Г.М.О.’,
+    ‘tracks’: [‘Последний месяц осени’, ‘Шапито’],
+    ‘Albums’: [{‘name’: ‘Делать панк-рок’,‘year’: 2016},
+    {‘name’: ‘Шапито’,‘year’: 2014}]}
+
+    С помощью модулей json и pickle сериализовать данный словарь в json и в байты,
+    вывести результаты в терминал. 
+    Записать результаты в файлы group.json, group.pickle соответственно.
+    В файле group.json указать кодировку utf-8.
+"""
+
+import json
+import pickle
+
+my_favourite_group = {
+    "name": "Г.М.О.",
+    "tracks": ["Последний месяц осени", "Шапито"],
+    "Albums": [{"name": "Делать панк-рок", "year": 2016},
+    {"name": "Шапито", "year": 2014}]}
+
+my_fav_group_json = json.dumps(my_favourite_group)
+print("JSON:", my_fav_group_json)
+
+my_fav_group_pickle = pickle.dumps(my_favourite_group)
+print("PICKLE:", my_fav_group_pickle)
+
+with open("group.json", "w", encoding="utf-8") as f_json:
+    f_json.write(my_fav_group_json)
+
+with open("group.pickle", "wb") as f_pickle:
+    f_pickle.write(my_fav_group_pickle)
