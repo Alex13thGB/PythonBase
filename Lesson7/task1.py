08760bfd673fb80701ba2edc84d256bfd560fd50
alex13th <alex13th76@gmail.com>
2020-04-29 14:40:45 +0800

    Lesson 7 is done

---
+++ Lesson7/task1.py
@@ -0,0 +1,12 @@
+"""
+Даны два списка фруктов. Получить список фруктов, присутствующих в обоих исходных списках. 
+ Примечание:
+   Списки фруктов создайте вручную в начале файла.
+"""
+
+fruits1 = ["apple", "grape", "papaya", "mango", "lemon", "pear", "avocado"]
+fruits2 = ["banana", "orange", "mango", "apple", "cherry", "grape", "lemon"]
+
+fruits = [fruit for fruit in fruits1 if fruit in fruits2]
+
+print(fruits)
